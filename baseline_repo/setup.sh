6ed14c3653e1eaf5fc2d7864435eed7c87556ca1
Debayan Ghosh <gdebayan95@gmail.com>
2022-03-31 17:27:25 -0400

    generalise paths in setup.sh

--- baseline_repo/setup.sh
+++ baseline_repo/setup.sh
@@ -1,22 +1,22 @@
 # """### **Dataset Download** """
 
-BASE_PATH=/home/ubuntu/MML
+BASE_PATH=/content/drive/MyDrive/MMML
 
-wget https://tvqa.cs.unc.edu/files/tvqa_qa_release.tar.gz
+wget https://tvqa.cs.unc.edu/files/tvqa_qa_release.tar.gz -P ${BASE_PATH}
 
-wget https://tvqa.cs.unc.edu/files/tvqa_subtitles.tar.gz
+wget https://tvqa.cs.unc.edu/files/tvqa_subtitles.tar.gz -P ${BASE_PATH}
 
-tar xzf tvqa_qa_release.tar.gz
+tar xzf ${BASE_PATH}/tvqa_qa_release.tar.gz -C ${BASE_PATH}
 
-tar xzf tvqa_subtitles.tar.gz
+tar xzf ${BASE_PATH}/tvqa_subtitles.tar.gz  -C ${BASE_PATH}
 
-wget https://tvqa.cs.unc.edu/files/tvqa_plus_annotations.tar.gz  -P ./tvqa_plus/
+wget https://tvqa.cs.unc.edu/files/tvqa_plus_annotations.tar.gz  -P ${BASE_PATH}/tvqa_plus/
 tar -xf ${BASE_PATH}/tvqa_plus/tvqa_plus_annotations.tar.gz -C ${BASE_PATH}/tvqa_plus/
 
-wget https://tvqa.cs.unc.edu/files/tvqa_plus_annotations_preproc_with_test.tar.gz  -P ./tvqa_plus/
+wget https://tvqa.cs.unc.edu/files/tvqa_plus_annotations_preproc_with_test.tar.gz  -P ${BASE_PATH}/tvqa_plus/
 tar -xf ${BASE_PATH}/tvqa_plus/tvqa_plus_annotations_preproc_with_test.tar.gz -C ${BASE_PATH}/tvqa_plus/
 
-wget https://tvqa.cs.unc.edu/files/tvqa_plus_subtitles.tar.gz  -P ./tvqa_plus/
+wget https://tvqa.cs.unc.edu/files/tvqa_plus_subtitles.tar.gz  -P ${BASE_PATH}/tvqa_plus/
 tar -xf ${BASE_PATH}/tvqa_plus/tvqa_plus_subtitles.tar.gz -C ${BASE_PATH}/tvqa_plus/
 
 pip install pysrt
